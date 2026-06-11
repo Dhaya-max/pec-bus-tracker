29661df12ab26de27b96b6a0d4d96b96ff70e5d6
Dhaya-max <dhayanithimanivannan@gmail.com>
2026-06-11 09:25:43 +0530

    fix: replace LoadScript with useJsApiLoader to fix white screen

--- src/components/BusMap.jsx
+++ src/components/BusMap.jsx
@@ -1,4 +1,4 @@
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api'
+import { GoogleMap, Marker, useJsApiLoader } from '@react-google-maps/api'
 
 const containerStyle = {
   width: '100%',
@@ -9,33 +9,41 @@ const containerStyle = {
 const defaultCenter = { lat: 13.0456, lng: 80.1021 }
 
 export default function BusMap({ busLocations = [] }) {
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: 'AIzaSyD2jKjetYt0Y9rg1zPuY94MVd07OHZnGlk'
+  })
+
+  if (!isLoaded) return (
+    <div className="h-40 flex items-center justify-center text-gray-400 text-sm bg-[#EFF6FF] rounded-xl">
+      Loading map...
+    </div>
+  )
+
   return (
-    <LoadScript googleMapsApiKey="AIzaSyD2jKjetYt0Y9rg1zPuY94MVd07OHZnGlk">
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={busLocations.length > 0 ? { lat: busLocations[0].lat, lng: busLocations[0].lng } : defaultCenter}
-        zoom={13}
-      >
-        {busLocations.map((loc, i) => (
-          <Marker
-            key={i}
-            position={{ lat: loc.lat, lng: loc.lng }}
-            icon={{
-              url: 'https://img.icons8.com/color/48/bus.png',
-              scaledSize: new window.google.maps.Size(40, 40),
-              anchor: new window.google.maps.Point(20, 20),
-              labelOrigin: new window.google.maps.Point(20, 48)
-            }}
-            label={{
-              text: loc.busNumber || 'Bus',
-              color: '#1E3A5F',
-              fontWeight: 'bold',
-              fontSize: '11px'
-            }}
-            title={loc.busNumber}
-          />
-        ))}
-      </GoogleMap>
-    </LoadScript>
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={busLocations.length > 0 ? { lat: busLocations[0].lat, lng: busLocations[0].lng } : defaultCenter}
+      zoom={13}
+    >
+      {busLocations.map((loc, i) => (
+        <Marker
+          key={i}
+          position={{ lat: loc.lat, lng: loc.lng }}
+          icon={{
+            url: 'https://img.icons8.com/color/48/bus.png',
+            scaledSize: new window.google.maps.Size(40, 40),
+            anchor: new window.google.maps.Point(20, 20),
+            labelOrigin: new window.google.maps.Point(20, 48)
+          }}
+          label={{
+            text: loc.busNumber || 'Bus',
+            color: '#1E3A5F',
+            fontWeight: 'bold',
+            fontSize: '11px'
+          }}
+          title={loc.busNumber}
+        />
+      ))}
+    </GoogleMap>
   )
 }
